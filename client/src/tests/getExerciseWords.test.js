6f80ef69f58ca0cc431d8709ce8365e59b4232b4
unknown <berkozzambak@gmail.com>
2022-11-08 16:40:05 +0100

    Create a mock User object to use in tests

--- client/src/tests/getExerciseWords.test.js
+++ client/src/tests/getExerciseWords.test.js
@@ -1,6 +1,9 @@
 const getExerciseWords = require('../helpers/getExerciseWords')
+const mockUser = require('./mockUserObject')
+
 
 test('imported succesfully', () => {
+    console.log(mockUser)
     expect(getExerciseWords()).toBe(1)
 })
 
---
+++ client/src/tests/mockUserObject.js
@@ -0,0 +1,58 @@
+
+module.exports = {
+      name: 'Generic User',
+      notebooks: {
+          Notebook100: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook101: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook102: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook103: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook104: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook105: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook106: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook107: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook108: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            },
+            Notebook109: {
+              wordCount: 200,
+              words: { type1: createGenericArray(), type2: createGenericArray(), type3: createGenericArray(), type4: createGenericArray() }
+            }
+      }
+  
+  }
+
+  const createGenericArray = () => {
+    let result = []
+    for(let i = 0; i < 50; i++){
+        result.push({original: 'Test', translation: 'Test', type: 'Test'})
+    }
+    return result
+  }
+  
+  
+  
